1a7c7f19151086fc61d8cd9ec2f528d13876614f
ffonseca <felipecfonseca@gmail.com>
2022-10-27 10:20:07 -0300

    Adding eu-west-1 TLS certificate for Load Balancer

--- terraform/acm.tf
+++ terraform/acm.tf
@@ -1,3 +1,18 @@
+# DEFAULT CERTIFICATE (EU-WEST-1)
+resource "aws_acm_certificate" "cert_ie" {
+  domain_name       = local.domain
+  validation_method = "DNS"
+
+  tags = merge(
+		{ Resource = "acm_certificate" },
+		local.tags
+	)
+
+  lifecycle {
+    create_before_destroy = true
+  }
+}
+
 # CERTIFICATE TO CLOUDFRONT (US-EAST-1 REQUIRED)
 resource "aws_acm_certificate" "cert_us" {
   provider = aws.us-east-1
